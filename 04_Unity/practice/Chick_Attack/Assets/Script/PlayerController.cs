48d50fc160e4eb3555fba886bee2ff6ca1549347
전신후 <sinhujun11@gmail.com>
2026-01-15 02:54:58 +0900

    chore: update moveable Range to 10.0

--- 04_Unity/practice/Chick_Attack/Assets/Script/PlayerController.cs
+++ 04_Unity/practice/Chick_Attack/Assets/Script/PlayerController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 8f; //속도
-    public float moveableRange = 5.5f; //이동 범위 제한
+    public float moveableRange = 10.0f; //이동 범위 제한
     public float power = 1000f; //포탄 위력
     public GameObject cannonBall; //발사할 포탄 설정
     public Transform spawnPoint; //발사 위치 설정
